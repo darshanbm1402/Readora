1a50449b9f9222cb02be96a372ef032e2fd76a50
darshanbm1402 <darshanbm00886@gmail.com>
2026-09-25 14:26:11 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://readora-backend-wpet.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
